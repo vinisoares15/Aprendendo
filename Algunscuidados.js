1e8b4382f2620eedc345747757f1f1595722493c
vinisoares15 <58192275+vinisoares15@users.noreply.github.com>
2020-08-01 21:26:24 -0300

    Aprendendo Fundamentos
    o básico de JS

---
+++ Algunscuidados.js
@@ -0,0 +1,8 @@
+console.log(7 / 0.0)  // dá infinito
+console.log('10'/2)  
+console.log('1' + 3)  //concatena pois a string tem preferÊncia
+console.log('1' - 3)  // não faz sentido concatenar, logo, faz a operação normalmente.
+console.log("Show" * 2)  //NaN
+console.log(0.1 + 0.7)   //
+//console.log(10.tostring())    //literal não funciona
+console.log((10.341).toFixed(2))  
